a0d1463e26df89e24f64571c6d2dab9e88bd6d67
Sagar <sagarmal624@gmail.com>
2018-08-15 10:04:32 +0530

    added some query for reference

--- src/main/java/com/sagarandcompany/CriteriaQuery/PersonService.java
+++ src/main/java/com/sagarandcompany/CriteriaQuery/PersonService.java
@@ -21,14 +21,18 @@ public class PersonService {
     }
 
     public Person get(Long id) {
-        return getSelectedColumnSinglePerson(id);
+        return getSinglePerson(id);
     }
 
     public Person getSinglePerson(Long id) {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Person> criteriQuery = criteriaBuilder.createQuery(Person.class);
+
         Root person = criteriQuery.from(Person.class);
+
         criteriQuery.where(criteriaBuilder.equal(person.get("id"), id));
+
+
         Query query = entityManager.createQuery(criteriQuery);
         Person person1 = (Person) query.getSingleResult();
         return person1;
@@ -38,8 +42,12 @@ public class PersonService {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = builder.createQuery(Object[].class);
         Root<Person> personRoot = criteriaQuery.from(Person.class);
+
         criteriaQuery.select(builder.array(personRoot.get("id"), personRoot.get("name")));
-        criteriaQuery.where(builder.equal(personRoot.get("email"), "salary"));
+
+        criteriaQuery.where(builder.equal(personRoot.get("email"), "rahul@gmail.com"));
+
+
         List<Object[]> dataArray = entityManager.createQuery(criteriaQuery).getResultList();
         for (Object[] values : dataArray) {
             Integer id = (Integer) values[0];
@@ -49,12 +57,13 @@ public class PersonService {
             System.out.println("id:" + id + ", name:" + name + " , email:" + email + " , salary:" + salary);
         }
     }
-
+////select deptno,count(id) from person group by deptno;
     public void groupBy() {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = builder.createQuery(Object[].class);
         Root<Person> personRoot = criteriaQuery.from(Person.class);
         criteriaQuery.select(builder.array(personRoot.get("deptno"), builder.count(personRoot.get("id"))));
+
         criteriaQuery.groupBy(personRoot.get("deptno"));
         List<Object[]> dataArray = entityManager.createQuery(criteriaQuery).getResultList();
         for (Object[] values : dataArray) {
@@ -64,15 +73,19 @@ public class PersonService {
         }
 
     }
-
+//select deptno,count(id) from person group by deptno having(count(id)>=2);
     public void groupByWithHaving() {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = builder.createQuery(Object[].class);
         Root<Person> personRoot = criteriaQuery.from(Person.class);
         criteriaQuery.select(builder.array(personRoot.get("deptno"), builder.count(personRoot.get("id"))));
         criteriaQuery.groupBy(personRoot.get("deptno"));
+
+
         Expression countExpression = builder.count(personRoot.get("id"));
+
         criteriaQuery.having(builder.gt(countExpression, 2));
+
         List<Object[]> dataArray = entityManager.createQuery(criteriaQuery).getResultList();
         for (Object[] values : dataArray) {
             Integer id = (Integer) values[0];
@@ -81,15 +94,18 @@ public class PersonService {
         }
 
     }
-
+  //select p.id,p.email,p.name,p.salary,a.city from person p inner join address a where p.address_id=a.id;
     public void fetchJoinBasedData() {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Object[]> criteriaQuery = builder.createQuery(Object[].class);
         Root<Person> personRoot = criteriaQuery.from(Person.class);
+
         Join addressRoot = personRoot.join("address");
+
+
         criteriaQuery.select(builder.array(personRoot.get("id"), personRoot.get("name"), addressRoot.get("city")));
-        criteriaQuery.where(builder.equal(personRoot.get("id"), 1));
-        criteriaQuery.where(builder.equal(addressRoot.get("id"), 2));
+        criteriaQuery.where(builder.equal(personRoot.get("address"), addressRoot.get("id")));
+//        criteriaQuery.where(builder.equal(addressRoot.get("id"), 2));
         List<Object[]> dataArray = entityManager.createQuery(criteriaQuery).getResultList();
         for (Object[] values : dataArray) {
             Long id = (Long) values[0];
@@ -104,8 +120,11 @@ public class PersonService {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Person> criteriQuery = criteriaBuilder.createQuery(Person.class);
         Root person = criteriQuery.from(Person.class);
+
         criteriQuery.select(criteriaBuilder.construct(Person.class, person.get("name"), person.get("email")));
+
         criteriQuery.where(criteriaBuilder.equal(person.get("id"), id));
+
         Query query = entityManager.createQuery(criteriQuery);
         Person person1 = (Person) query.getSingleResult();
         return person1;
@@ -127,7 +146,9 @@ public class PersonService {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Integer> criteriaQuery = builder.createQuery(Integer.class);
         Root<Person> studentRoot = criteriaQuery.from(Person.class);
+
         criteriaQuery.select(builder.max(studentRoot.get("salary").as(Integer.class)));
+
         Integer maxSalary = entityManager.createQuery(criteriaQuery).getSingleResult();
         System.out.println("maxSalary" + maxSalary);
     }
